41d20eac43cf56e9f1d1e7343b01d679ac8395c1
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-22 20:28:06 -0400

    server can start and show stuff

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// TODO: Add a comment describing the functionality of the withAuth middleware
+
 router.get('/', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
@@ -14,7 +14,6 @@ router.get('/', withAuth, async (req, res) => {
 
     res.render('homepage', {
       users,
-      // TODO: Add a comment describing the functionality of this property
       logged_in: req.session.logged_in,
     });
   } catch (err) {
---
+++ server.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.get("/", (req,res) => {
+    res.render("homepage")
+})
+
+
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+  
