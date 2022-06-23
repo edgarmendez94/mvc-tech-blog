fea4582a7998ed3564509e8ca99242886c89700c
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-22 22:20:26 -0400

    added model

---
+++ models/Blog.js
@@ -0,0 +1,42 @@
+const {Model, Datatypes} = require("sequelize");
+const sequelize = require("../config/connection");
+
+
+
+class Blog extends Model {}
+
+Blog.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      post_by: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      post_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'blog',
+    }
+  );
+  
+  module.exports = Blog;
+  
