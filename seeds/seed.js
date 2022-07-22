cecba94318cb500b528aeb2495500e35b0cdc52c
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-21 23:51:23 -0400

    small amount of seeds being made

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,14 @@
 const sequelize = require('../config/connection');
-const seedBlog = require('./blogData');
-
+const {User,Blog,Comment} = require('../models');
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-
-  await seedBlog();
-
+  await User.bulkCreate(userData, {});
+  await Blog.bulkCreate(blogData, {});
+  await Comment.bulkCreate(commentData, {});
   process.exit(0);
 };
 
