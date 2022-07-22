410f54a375cf31583233af4fd092d1d4018edc44
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-21 22:07:43 -0400

    server starts , dot env installed, server js fixed

---
+++ utils/helpers.js
@@ -0,0 +1,8 @@
+module.exports = {
+    format_date: (date) => {
+      return `${new Date(date).getMonth()}/${new Date(date).getDate()}/${new Date(
+        date
+      ).getFullYear()}`;
+    },
+  };
+  
